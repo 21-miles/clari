dd6025bb983f0d22e9363bccfa849a61a807a4b1
21 Miles - AI and Web Solutions <contato@edu4.dev>
2025-03-07 23:23:12 +0000

    bugfix/imgs deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
-  reactStrictMode: false,
-  unoptimized: true,
-  trailingSlash: true,
-  assetPrefix: isProd ? process.env.WEBSITE_URL : "",
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
